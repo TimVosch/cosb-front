2e2c9329c95b5bed52accd133ea925903b79d1cc
TimVosch <timbox@live.nl>
2016-06-16 21:34:37 +0200

    Set up component structure

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Sentinel Panel<small><br/>We're working on it :)</small></h1>
-            </div>
-        );
-    }
-}
+import Layout from './components/Layout'
+
 
 const app = document.getElementById('app');
-
 ReactDOM.render(<Layout/>, app);
---
+++ src/js/components/Layout.js
@@ -0,0 +1,9 @@
+export default class Layout extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Sentinel Panel<small><br/>We're working on it :)</small></h1>
+            </div>
+        );
+    }
+}
