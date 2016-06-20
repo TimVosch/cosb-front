352220a8bf3521ebedbb61e6c2091f525eae39f7
TimVosch <timbox@live.nl>
2016-06-21 00:06:59 +0200

    Font-awesome included as npm module

--- src/js/entry.js
+++ src/js/entry.js
@@ -3,6 +3,6 @@
 require('../bootstrap/theme/css/sb-admin-2.css');
 require('../bootstrap/theme/css/timeline.css');
 require('../bootstrap/theme/js/sb-admin-2.js');
-require('../style/css/font-awesome.css');
+require('font-awesome/css/font-awesome.min.css');
 
 import App from './app.js';
