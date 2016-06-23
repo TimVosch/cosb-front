a68a7bf51e21d93cd0e7144403686fd63af49e18
TimVosch <timbox@live.nl>
2016-06-23 23:15:37 +0200

    Update for production

--- webpack.config.js
+++ webpack.config.js
@@ -40,5 +40,8 @@ module.exports = {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.DefinePlugin({
+      "sentinel.config": SentinelConfiguration
+    })
   ],
 };
