7b1f9ff4f7f64a893abef6d58b2bac6305fc9e15
TimVosch <timbox@live.nl>
2016-06-23 16:41:23 +0200

    Removed font-awesome will reimplement

--- src/js/entry.js
+++ src/js/entry.js
@@ -3,6 +3,5 @@
 require('../bootstrap/theme/css/sb-admin-2.css');
 require('../bootstrap/theme/css/timeline.css');
 require('../bootstrap/theme/js/sb-admin-2.js');
-require('font-awesome/css/font-awesome.min.css');
 
 import App from './app.js';
