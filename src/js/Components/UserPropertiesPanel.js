462a2833dc01d98e8eeb583eaa05b4ff7ea102de
TimVosch <timbox@live.nl>
2016-06-26 21:40:21 +0200

    added use role layout to properties panel

--- src/js/Components/UserPropertiesPanel.js
+++ src/js/Components/UserPropertiesPanel.js
@@ -5,7 +5,11 @@ export default class UserPropertiesPanel extends React.Component {
     constructor() {
         super();
         this.state = {
-            user: {}
+            user: {
+                uuid: undefined,
+                username: undefined,
+                role: undefined
+            }
         };
     }
     
@@ -27,12 +31,23 @@ export default class UserPropertiesPanel extends React.Component {
                 <div class="panel-heading">
                     <strong>User properties</strong>
                 </div>
-                <div class="panel-body">
+                <div class="panel-body" style={{display: (!this.state.user.uuid && !this.state.user.username)? "none":"block"}}>
                     <h3 style={{marginBottom: "5px", marginTop: "0"}}>{this.state.user.username}<small><br/>{this.state.user.uuid}</small></h3><hr style={{marginTop: "0"}}/>
-                    
                     <button class="btn btn-default btn-xs pull-right" data-toggle="collapse" data-target="#crafatar-image">Toggle <span class="caret"></span></button>
                     <div id="crafatar-image" class="collapse in">
                         <img src={"https://crafatar.com/renders/body/" + this.state.user.uuid} class="responsive center-block" style={{maxHeight: "200px", display: (this.state.user.uuid !== undefined)? "block" : "none"}} />
+                        <hr/>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <strong>User role:</strong>
+                        </div>
+                        <div class="col-sm-6">
+                            {(this.state.user.role || "N/A")}
+                        </div>                    
+                    </div>
+                    <div class="row">
+                        
                     </div>
                 </div>
             </div>
